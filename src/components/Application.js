94ee82dc93dceba8faa7804b038ec3e859551002
affafrai <affaf-rai@hotmail.com>
2020-10-02 16:21:50 -0400

    updated the apllication.js to render the days from api using useeffect with axios request as a side effect

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-
+import React, {useState, useEffect} from "react";
+import axios from "axios"
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
@@ -43,69 +43,59 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 const setName = name => console.log(name);
 setName("affaf");
 
 const renderAppointment = (appointment) => {
   const {id,time,interview} = appointment;
-
   return (
-
-
-        <Appointment key={id} {...appointment}/>
-        
-
+    <Appointment key={id} {...appointment}/>    
   )
 }
 
 export default function Application(props) {
-  const[day, setDay] = useState("Monday")
-    return (
-      <main className="layout">
-        <section className="sidebar">
-          <img
-            className="sidebar--centered"
-            src="images/logo.png"
-            alt="Interview Scheduler"
-          />
-          <hr className="sidebar__separator sidebar--centered" />
-          <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            day={day}
-            setDay={setDay}
-          />
-          </nav>
-          <img
-            className="sidebar__lhl sidebar--centered"
-            src="images/lhl.png"
-            alt="Lighthouse Labs"
-          />
-        </section>
-        <section className="schedule">
+  const[day, setDay] = useState([])
+  const[results, setResults] = useState([])
 
-       {appointments.map(renderAppointment)}
-       <Appointment key="last" time="5pm" />
 
-        </section>
-      </main>
-    );  
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(response => {
+      setResults(response.data)
+      console.log("response.data",response.data)
+    })
+    .catch(error => {console.error(error.message)} );
+  }, [])
+
+  return (
+    <main className="layout">
+      <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+        <DayList
+          days={results}
+          day={day}
+          setDay={setDay}
+        />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+      </section>
+      <section className="schedule">
+
+      {appointments.map(renderAppointment)}
+      <Appointment key="last" time="5pm" />
+
+      </section>
+    </main>
+  );  
 }
